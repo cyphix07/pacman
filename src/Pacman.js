bcb1956a8304ccf2f0b555ea80e8b4c2f85220e2
cyphix07 <cyphix07@gmail.com>
2024-01-28 02:56:51 +0530

    done with the collision detection

---
+++ src/Pacman.js
@@ -0,0 +1,107 @@
+import MovingDirection from "./MovingDirection.js";
+
+export default class Pacman {
+  constructor(x, y, tileSize, velocity, tileMap) {
+    this.x = x;
+    this.y = y;
+    this.tileSize = tileSize;
+    this.velocity = velocity;
+    this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
+    this.#loadPacmanImages();
+  }
+
+  draw(ctx) {
+    this.#move();
+    ctx.drawImage(
+      this.pacmanImages[this.pacmanImageIndex],
+      this.x,
+      this.y,
+      this.tileSize,
+      this.tileSize,
+    );
+  }
+
+  #loadPacmanImages() {
+    const pacmanImage1 = new Image();
+    pacmanImage1.src = "../images/pac0.png";
+
+    const pacmanImage2 = new Image();
+    pacmanImage2.src = "../images/pac1.png";
+
+    const pacmanImage3 = new Image();
+    pacmanImage3.src = "../images/pac2.png";
+
+    const pacmanImage4 = new Image();
+    pacmanImage4.src = "../images/pac1.png";
+
+    this.pacmanImages = [
+      pacmanImage1,
+      pacmanImage2,
+      pacmanImage3,
+      pacmanImage4,
+    ];
+    this.pacmanImageIndex = 2;
+  }
+  #keydown = (event) => {
+    //this is the up key
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down)
+        this.currentMovingDirection = MovingDirection.up;
+      this.requestedMovingDirection = MovingDirection.up;
+    }
+    //this is the down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up)
+        this.currentMovingDirection = MovingDirection.down;
+      this.requestedMovingDirection = MovingDirection.down;
+    }
+    // this is the left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right)
+        this.currentMovingDirection = MovingDirection.left;
+      this.requestedMovingDirection = MovingDirection.left;
+    }
+    // this is the right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left)
+        this.currentMovingDirection = MovingDirection.right;
+      this.requestedMovingDirection = MovingDirection.right;
+    }
+  };
+  #move() {
+    if (this.currentMovingDirection !== this.requestedMovingDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        if (
+          !this.tileMap.didCollideWithEnviornment(
+            this.x,
+            this.y,
+            this.requestedMovingDirection,
+          )
+        )
+          this.currentMovingDirection = this.requestedMovingDirection;
+      }
+    }
+    switch (this.currentMovingDirection) {
+      case MovingDirection.up:
+        this.y -= this.velocity;
+        break;
+      case MovingDirection.down:
+        this.y += this.velocity;
+        break;
+      case MovingDirection.left:
+        this.x -= this.velocity;
+        break;
+      case MovingDirection.right:
+        this.x += this.velocity;
+        break;
+    }
+  }
+}
